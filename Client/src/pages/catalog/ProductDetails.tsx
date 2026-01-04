92df957386cfa44029834f70e59df14aada3a50d
Melih Dincer <m_dincer41@hotmail.com>
2026-01-04 19:36:19 +0300

    Bölüm 5.4

--- Client/src/pages/catalog/ProductDetails.tsx
+++ Client/src/pages/catalog/ProductDetails.tsx
@@ -1,5 +1,51 @@
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography, Box } from "@mui/material";
+import SearchOffIcon from "@mui/icons-material/SearchOff";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { IProduct } from "../../model/IProduct";
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product Detail</Typography>;
+  const { id } = useParams();
+  const [product, setProduct] = useState<IProduct | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`http://localhost:5047/api/Products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]); //sadece id değiştiğinde fetch request yapılacak.
+
+  if (loading)
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="50vh"
+      >
+        <CircularProgress size={40} />
+      </Box>
+    );
+  if (!product)
+    return (
+      <Box
+        textAlign="center"
+        mt={6}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <SearchOffIcon sx={{ fontSize: 80, color: "text.disabled", mb: 2 }} />
+        <Typography variant="h5" fontWeight={600}>
+          Ürün Bulunamadı
+        </Typography>
+        <Typography variant="body1" color="text.secondary" mt={1}>
+          Aradığınız ürün mevcut değil ya da kaldırılmış olabilir.
+        </Typography>
+      </Box>
+    );
+  if (!product) return <h5>Product not found...</h5>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
